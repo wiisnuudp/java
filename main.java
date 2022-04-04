3a3caf683583d1e2a63a9037bfdcd700d4f3b876
wiisnuudp <wisnudewa2000@gmail.com>
2022-04-04 10:30:12 +0700

    java learning

---
+++ main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class main{
+
+
+    
+    public static void main(String[] args) {
+    // String nik = "2153210412000001", nama = "WISNU DEWASYAH PUTRA", ttl = "SURABAYA, 04-12-2000",
+    // alamat = "Gading Kirana E",gol = "A";
+    // int umur = 21;
+
+    //     System.out.println("NIK : "+nik+"\nNama : "+nama+
+    //     "\nTTL : "+ ttl +"\nUmur :"+umur+"\nAlamat : "+alamat+
+    //     "\nGOL : "+gol);
+
+    // Scanner nilai = new Scanner(System.in);
+    // System.out.print("masukkan nilai : ");
+    // int i = nilai.nextInt();
+    // String grade;
+    
+
+    // if(i >= 90){
+
+    //     grade = "A";
+    // }
+    // else if(i >= 80){
+
+    //     grade = "B";
+    // }
+    // else if(i >= 70){
+    //     grade = "C";
+    // }
+    // else if(i >= 60){
+    //     grade = "D";
+    // } else {
+    //     grade = "E";
+    // }
+
+    // System.out.print("Nilai anda : "+grade);
+
+    int a = 7;
+    
+    for(int i = 0; i<a;i = i+2){
+        for(int u=0;u<=i;u++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+    }
+
+    
+}
